ec93bfa4bf929e89422bc8ebc66678457ec15c54
Dr. Corona <dr.corona@the-imperfection.de>
2020-03-21 17:20:29 +0000

    Deploying to gh-pages from  - c1003cf70d8e99f2af56295741603749f97c04ad 🚀

---
+++ add-swiping-tinder-cards/precache-manifest.7c4d4b842920fbbedf9cf66acd08843b.js
@@ -0,0 +1,414 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4aea9047e2d2aee0007128e8aea04a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/index.html"
+  },
+  {
+    "revision": "1822e05f79936ed42821",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/css/8.1594e46e.chunk.css"
+  },
+  {
+    "revision": "33d7890f00ba405095a8",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/css/main.d9010cce.chunk.css"
+  },
+  {
+    "revision": "fe857fe5adef5d93e501",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/0.c59c7b57.chunk.js"
+  },
+  {
+    "revision": "fd2d371e07b10b66f2e8",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/1.b9b5a542.chunk.js"
+  },
+  {
+    "revision": "d609da053d4d7a3b81d7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/10.176853b7.chunk.js"
+  },
+  {
+    "revision": "181fd4a1fb7d6023c363",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/11.6d080b50.chunk.js"
+  },
+  {
+    "revision": "b22749e0c8173cb6e5e3",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/12.c1f07e97.chunk.js"
+  },
+  {
+    "revision": "a73145b193dc50f654be",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/13.99ad859e.chunk.js"
+  },
+  {
+    "revision": "b0968944685c1dc12cbb",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/14.f7cf5d69.chunk.js"
+  },
+  {
+    "revision": "1393ad449f97b78b60af",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/15.887cb568.chunk.js"
+  },
+  {
+    "revision": "37d4c6d687d7149d7014",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/16.7107bbc0.chunk.js"
+  },
+  {
+    "revision": "e2d0573809c1098f7a1f",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/17.9694bb8f.chunk.js"
+  },
+  {
+    "revision": "1729e9fe9afb8b838653",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/18.c19d1e9e.chunk.js"
+  },
+  {
+    "revision": "df36e17a625c3f20e7e5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/19.3e45681c.chunk.js"
+  },
+  {
+    "revision": "8780d16765d479164939",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/2.6f208e3c.chunk.js"
+  },
+  {
+    "revision": "7c2f2cb2b8c5d38f3696",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/20.715f2dcf.chunk.js"
+  },
+  {
+    "revision": "fbad3b27c9c8bc666363",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/21.54d8bd1f.chunk.js"
+  },
+  {
+    "revision": "a5f405d38dda788eaff9",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/22.2e93a025.chunk.js"
+  },
+  {
+    "revision": "09b7e7f56afb6f91201a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/23.a087bbaf.chunk.js"
+  },
+  {
+    "revision": "5190edd228696e1c80a1",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/24.dab26366.chunk.js"
+  },
+  {
+    "revision": "7f0c736a00efd8bb7aa0",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/25.5f26a598.chunk.js"
+  },
+  {
+    "revision": "37fee2feffb7804454d4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/26.73557abd.chunk.js"
+  },
+  {
+    "revision": "239d3396cc0131bb5121",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/27.405310f8.chunk.js"
+  },
+  {
+    "revision": "f170f3daec7453c90511",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/28.24f19711.chunk.js"
+  },
+  {
+    "revision": "954d20ce798a890f6462",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/29.6487eb2f.chunk.js"
+  },
+  {
+    "revision": "d31df8378f95a6a31a52",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/3.d6547f8b.chunk.js"
+  },
+  {
+    "revision": "ca1bbd5e26bc52adebd1",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/30.274cb0fd.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/30.274cb0fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4075ed11ffe6c8fa085c",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/31.3440a8e8.chunk.js"
+  },
+  {
+    "revision": "febb2a0395ff83888e84",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/32.6764b473.chunk.js"
+  },
+  {
+    "revision": "9d01d5f2c4e25cd31811",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/33.d3d9d969.chunk.js"
+  },
+  {
+    "revision": "22423ad57b53d8fd18f4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/34.58caa184.chunk.js"
+  },
+  {
+    "revision": "db8d2ae7c2bf1d97c09d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/35.ce4bafcf.chunk.js"
+  },
+  {
+    "revision": "869b1b06d424bf6e38f7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/36.3ce01b6b.chunk.js"
+  },
+  {
+    "revision": "f0c1d0b19d2d383601b1",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/37.c59270e7.chunk.js"
+  },
+  {
+    "revision": "e36d5124479c2e96c927",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/38.b68d15e2.chunk.js"
+  },
+  {
+    "revision": "8451da80dfb4acd35a0d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/39.4dc922bc.chunk.js"
+  },
+  {
+    "revision": "593812476aa3426f46ee",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/4.a55ff1a6.chunk.js"
+  },
+  {
+    "revision": "57e38abdf1e4cf82b4ab",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/40.e8a07ba5.chunk.js"
+  },
+  {
+    "revision": "2577e48ae9adf6693c5a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/41.4d36ae29.chunk.js"
+  },
+  {
+    "revision": "061293005433b84026a5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/42.6cd19418.chunk.js"
+  },
+  {
+    "revision": "953ab0b5424e33a9e85d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/43.a4d5e6f8.chunk.js"
+  },
+  {
+    "revision": "7f0f1c28edc29a104e7d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/44.7cc4c2f8.chunk.js"
+  },
+  {
+    "revision": "9a65b43149c1271dde13",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/45.9932299f.chunk.js"
+  },
+  {
+    "revision": "81ef3e3d887e0ee4693a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/46.934eba8b.chunk.js"
+  },
+  {
+    "revision": "b75a5119d73fd3f35de6",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/47.70c0cf05.chunk.js"
+  },
+  {
+    "revision": "fe6b9e51c3a716a52426",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/48.398ebc8e.chunk.js"
+  },
+  {
+    "revision": "705e2554a4cdf6d2dfdc",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/49.74684179.chunk.js"
+  },
+  {
+    "revision": "302005418cc464d019f6",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/5.be8df34e.chunk.js"
+  },
+  {
+    "revision": "d92275865e3e11382960",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/50.c69724e3.chunk.js"
+  },
+  {
+    "revision": "b4b4b8697acf7ce3da93",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/51.59298822.chunk.js"
+  },
+  {
+    "revision": "04fb63d981d2ecd36c15",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/52.10b44c34.chunk.js"
+  },
+  {
+    "revision": "48cf4c99bf04cb26be5f",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/53.0f0d7ead.chunk.js"
+  },
+  {
+    "revision": "94171bbc10a71b310ce5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/54.d6946aee.chunk.js"
+  },
+  {
+    "revision": "d94596e6a5ff3d2029d3",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/55.5dcd4cb3.chunk.js"
+  },
+  {
+    "revision": "0590fba48f432feaea99",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/56.fd2ba086.chunk.js"
+  },
+  {
+    "revision": "703e39f3252d534a5fe8",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/57.60616885.chunk.js"
+  },
+  {
+    "revision": "20b04940a306566e5727",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/58.489f2109.chunk.js"
+  },
+  {
+    "revision": "ed3c7f3f53c2c9b70607",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/59.e4cda42c.chunk.js"
+  },
+  {
+    "revision": "dd0d508cf3acd23dbe80",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/60.d261fb0d.chunk.js"
+  },
+  {
+    "revision": "9634395f64fe60079b09",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/61.ee20e9ca.chunk.js"
+  },
+  {
+    "revision": "970e3b87b047e81e35b4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/62.59a72ab7.chunk.js"
+  },
+  {
+    "revision": "1532003a56804985f228",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/63.22c71209.chunk.js"
+  },
+  {
+    "revision": "8996233f1cce5367bdac",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/64.240e2ddc.chunk.js"
+  },
+  {
+    "revision": "1a6088725f099a9ba546",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/65.2b99dea5.chunk.js"
+  },
+  {
+    "revision": "4aba9bb592f121aee7e0",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/66.ba885b6f.chunk.js"
+  },
+  {
+    "revision": "f94fcb3fb212dfcaa858",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/67.2505c8bb.chunk.js"
+  },
+  {
+    "revision": "4a1d7182889d9b82ce5b",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/68.0ccdbcbe.chunk.js"
+  },
+  {
+    "revision": "f94aa8d6e8af9c9f546b",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/69.3823050c.chunk.js"
+  },
+  {
+    "revision": "0b0773f0ec06fe7726bc",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/70.b7460942.chunk.js"
+  },
+  {
+    "revision": "dc1b3ee076e5598bf897",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/71.5e1ab02c.chunk.js"
+  },
+  {
+    "revision": "20d9c525b391789c97ee",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/72.3f06fd1e.chunk.js"
+  },
+  {
+    "revision": "affdccfdcc630892f4df",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/73.6d5c0969.chunk.js"
+  },
+  {
+    "revision": "24f486b7cdfaff176bd7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/74.4d1d8252.chunk.js"
+  },
+  {
+    "revision": "ef7d58c072806aee78dd",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/75.47fe1cd7.chunk.js"
+  },
+  {
+    "revision": "285b32408ab2616e498d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/76.6e1dc822.chunk.js"
+  },
+  {
+    "revision": "95679e79d5de7c07843e",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/77.51df7800.chunk.js"
+  },
+  {
+    "revision": "7ae06c393fec2f4c052a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/78.4bf68659.chunk.js"
+  },
+  {
+    "revision": "89debd690b83d67794c7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/79.136c9bfa.chunk.js"
+  },
+  {
+    "revision": "1822e05f79936ed42821",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/8.1e27f362.chunk.js"
+  },
+  {
+    "revision": "0229f2113b78559ca1daeb1dd663a5a5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/8.1e27f362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "addb401130f78c663bc0",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/80.c984be06.chunk.js"
+  },
+  {
+    "revision": "15144416e47bf2e83c79",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/81.659d06c5.chunk.js"
+  },
+  {
+    "revision": "9ee378dd29dd791ef3e2",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/82.496f5043.chunk.js"
+  },
+  {
+    "revision": "e376c23208774d0846a9",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/83.cb37ba1f.chunk.js"
+  },
+  {
+    "revision": "4fb6f258650ea3195d98",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/84.92637cfd.chunk.js"
+  },
+  {
+    "revision": "a622020982adbc629051",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/85.0d840df1.chunk.js"
+  },
+  {
+    "revision": "0e14e51baf5eadb1d35f",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/86.f44db3ae.chunk.js"
+  },
+  {
+    "revision": "16d6cf81b1c065816783",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/87.da3593b8.chunk.js"
+  },
+  {
+    "revision": "54ae0e27592e53a6479f",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/88.1ff14bd0.chunk.js"
+  },
+  {
+    "revision": "93e7418845c5ef95cf97",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/89.534fbc8a.chunk.js"
+  },
+  {
+    "revision": "f4747892be83688626b3",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/9.e98b92d0.chunk.js"
+  },
+  {
+    "revision": "58ffb9ca4d2b6368d083",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/90.b0cc9e61.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/90.b0cc9e61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c27d62259af0bf8f72",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/91.3fded334.chunk.js"
+  },
+  {
+    "revision": "c2ab6ccb2f599191c949",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/92.361007ec.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/92.361007ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e278872c085249a5c34f",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/93.a8de79c4.chunk.js"
+  },
+  {
+    "revision": "bdfa4f8ecf098c8a099a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/94.6643fbd9.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/94.6643fbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33d7890f00ba405095a8",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/main.f04bdf8b.chunk.js"
+  },
+  {
+    "revision": "ad1478b8cd777b7096e8",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/add-swiping-tinder-cards/static/js/runtime-main.9120c8f0.js"
+  }
+]);
---
+++ add-swiping-tinder-cards/static/js/3.d6547f8b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpswipe-the-curve"]=this["webpackJsonpswipe-the-curve"]||[]).push([[3],{488:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(54),o=(e(8),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.l)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=3.d6547f8b.chunk.js.map
