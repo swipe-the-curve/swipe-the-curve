218feb07c14c1efc7b53186737fbe1e858677577
Frederik Petersen <frederik@the-imperfection.de>
2020-03-21 13:10:04 +0100

    Removed files in root

--- static/js/3.7267655d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpswipe-the-curve"]=this["webpackJsonpswipe-the-curve"]||[]).push([[3],{488:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(53),o=(e(8),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.l)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=3.7267655d.chunk.js.map
