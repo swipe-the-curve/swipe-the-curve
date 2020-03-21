100fe10737cc4b4b061e28300970130149d529e2
Thomas Dissert <thomas.dissert@abusix.com>
2020-03-21 11:32:57 +0100

    added basic curve with reacharts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonIcon } from '@ionic/react';
 import { heart, people, cash } from 'ionicons/icons'
+import Curve from '../components/Curve';
 import React from 'react';
 import './Home.css';
 
@@ -25,7 +26,7 @@ const Home: React.FC = () => {
             </IonCol>
           </IonRow>
           <IonRow className="curve-container">
-            Curve
+            <Curve />
           </IonRow>
           <IonRow className="chartstack-container">
             <IonCol>Chartstack</IonCol>
