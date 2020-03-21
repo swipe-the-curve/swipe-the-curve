758577340e7caab6db4a86a8ceb669b2cd0e2eac
Andrea Bittner <github@andbit.de>
2020-03-21 01:00:07 +0100

    Initial layout with grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonRow, IonCol, IonIcon } from '@ionic/react';
+import { heart, people, cash } from 'ionicons/icons'
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -8,16 +8,29 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Swipe to #FlattenTheCurve</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+          <IonRow className="resource-container">
+            <IonCol>
+              <IonIcon icon={heart} className="iconheart"></IonIcon>
+            </IonCol>
+            <IonCol>
+              <IonIcon icon={people} className="iconpeople" ></IonIcon>
+            </IonCol>
+            <IonCol>
+              <IonIcon icon={cash} className="iconcash" ></IonIcon>
+            </IonCol>
+          </IonRow>
+          <IonRow className="curve-container">
+            Curve
+          </IonRow>
+          <IonRow className="chartstack-container">
+            <IonCol>Chartstack</IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
