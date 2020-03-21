d32f0737302184f81d5adb162b6b64f7ef84bc31
Dr. Corona <dr.corona@the-imperfection.de>
2020-03-21 11:40:35 +0000

    Deploying to gh-pages from  - 4dbe14313a49e98e9aef1fca00ee25db446f4ffe 🚀

--- master/service-worker.js
+++ master/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swipe-the-curve/master/precache-manifest.1e42e5fe2b9df8f20778871429c5e73a.js"
+  "/precache-manifest.a4272c52d95af4d4cb0436c5e4a12a34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swipe-the-curve/master/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
