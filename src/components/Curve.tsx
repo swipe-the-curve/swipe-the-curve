b5742bc8a64e07b786ac61cbd4b5e96a0024c9d4
Thomas Dissert <thomas.dissert@abusix.com>
2020-03-22 16:32:14 +0100

    added legend to graph

--- src/components/Curve.tsx
+++ src/components/Curve.tsx
@@ -1,6 +1,6 @@
 import { } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
-import { ResponsiveContainer, Area, Tooltip, ComposedChart, Line} from 'recharts';
+import { ResponsiveContainer, Area, Tooltip, ComposedChart, Line, Legend} from 'recharts';
 import { game } from '../core';
 
 const Curve: React.FC = () => {
@@ -34,7 +34,7 @@ const Curve: React.FC = () => {
   const currentImpacted = epidemicState && epidemicState.length > 0 && epidemicState[epidemicState.length - 1].infected + epidemicState[epidemicState.length - 1].recovered + epidemicState[epidemicState.length - 1].dead || 0;
 
   return (
-    <div className="full-width">
+    <div className="full-width full-height">
         <ResponsiveContainer height='100%' width='100%'>
           <ComposedChart
             data={epidemicState}
@@ -44,6 +44,7 @@ const Curve: React.FC = () => {
             }}
           >
             <Tooltip />
+            <Legend verticalAlign="top" height={36}/>
             <Area isAnimationActive={false} type="monotone" dataKey="infected" stackId="1" stroke="#f53d3d" fill="#f53d3d" />
             <Area isAnimationActive={false} type="monotone" dataKey="recovered" stackId="1" stroke="#4db374" fill="#4db374" />
             <Area isAnimationActive={false} type="monotone" dataKey="dead" stackId="1" stroke="#3b1111" fill="#3b1111" />
