f11cb2d11e79e530e739de22683a8d709d8f9680
Frederik Petersen <frederik@the-imperfection.de>
2020-03-22 20:47:14 +0100

    Added title for event cards

--- scripts/create_cards_json.py
+++ scripts/create_cards_json.py
@@ -15,7 +15,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 SPREADSHEET_ID = '1rsk73_oCuZdysI27fBRy5_sEkSR3oHR1aVG0aiP1aDE'
-RANGE_NAME = 'A9:K39'
+RANGE_NAME = 'TODO'
 
 creds = None
 # The file token.pickle stores the user's access and refresh tokens, and is
@@ -76,7 +76,7 @@ for row in result['values']:
     }
     cards.append(card)
 
-RANGE_NAME = 'A52:G79'
+RANGE_NAME = 'TODO'
 
 result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
                             range=RANGE_NAME,
--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { IonCard, IonCardContent } from '@ionic/react';
-import { Card } from '../core/domain/card';
+import { Card, EventCard } from '../core/domain/card';
 
 interface CardProps {
     card: Card | undefined
 }
 const CardComponent: React.FunctionComponent<CardProps> = (props) => {
     return (<div className="card shadow">
+        {props.card && props.card.id !== "E0" && props.card instanceof EventCard &&
+            <div className="eventtitle">Ereignis:</div>
+        }
         {props.card && props.card.text}
     </div>
     );
