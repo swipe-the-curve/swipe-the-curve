1e32202b65119460e1adb5ab533ba34df47fee1a
Dr. Corona <dr.corona@the-imperfection.de>
2020-03-22 22:45:24 +0000

    Deploying to gh-pages from  - d114e33fa8973a31233bf5b3be598925407a2926 🚀

--- add-random-events/service-worker.js
+++ add-random-events/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://swipe-the-curve.github.io/swipe-the-curve/add-random-events/precache-manifest.3b238880d03f017429382a12138387c9.js"
+  "https://swipe-the-curve.github.io/swipe-the-curve/add-random-events/precache-manifest.1b56c7a0eddc18674c23a037a567f392.js"
 );
 
 self.addEventListener('message', (event) => {
