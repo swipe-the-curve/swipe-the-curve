8a5fc704ed948bd5e6345bc7f3ba7f1b55259917
Dr. Corona <dr.corona@the-imperfection.de>
2020-03-22 12:46:28 +0000

    Deploying to gh-pages from  - 11d8b8f34bc32fe46d54bcbce9a96d6afa3a5894 🚀

---
+++ forecast/precache-manifest.da025b5a248a1e3a191b9398d0eccbc1.js
@@ -0,0 +1,414 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b688bc0f23c6851979eed78112cc2f66",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/index.html"
+  },
+  {
+    "revision": "51ff675cd39f86824a77",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/css/8.1594e46e.chunk.css"
+  },
+  {
+    "revision": "9b9de0aacf738ae3b4a7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/css/main.2f4a5de9.chunk.css"
+  },
+  {
+    "revision": "514ded7c593829d10697",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/0.a3f14c64.chunk.js"
+  },
+  {
+    "revision": "0aa1c9389b2bacf8dacc",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/1.3072e032.chunk.js"
+  },
+  {
+    "revision": "da0b813f5c354c1d019b",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/10.6f229b0b.chunk.js"
+  },
+  {
+    "revision": "2d151326d63ae023d912",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/11.916bcae7.chunk.js"
+  },
+  {
+    "revision": "4871b056fab5e28f2a16",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/12.b2e600a6.chunk.js"
+  },
+  {
+    "revision": "5cde9ee13803911b30b5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/13.1ca61caa.chunk.js"
+  },
+  {
+    "revision": "c2b82c234759aa29f98e",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/14.5b89aa10.chunk.js"
+  },
+  {
+    "revision": "79c6aa017626e1e0ccd9",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/15.0f524433.chunk.js"
+  },
+  {
+    "revision": "e930e9f8cc2928009b92",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/16.56cfd161.chunk.js"
+  },
+  {
+    "revision": "67a9a5766e8731ca3b6f",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/17.55dab44a.chunk.js"
+  },
+  {
+    "revision": "30a7efb9f6b9ca715027",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/18.bac80040.chunk.js"
+  },
+  {
+    "revision": "7032ab7f9bf62ec0b7c3",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/19.44167ef3.chunk.js"
+  },
+  {
+    "revision": "28955e03dd29424107cd",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/2.be3463a1.chunk.js"
+  },
+  {
+    "revision": "ad2cd986b9740af9c712",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/20.f811d0ea.chunk.js"
+  },
+  {
+    "revision": "62ac731433fd8800f288",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/21.47a89bfc.chunk.js"
+  },
+  {
+    "revision": "be0d06847ce903a465da",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/22.f4854f49.chunk.js"
+  },
+  {
+    "revision": "72910ff78801635606c3",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/23.d3f97067.chunk.js"
+  },
+  {
+    "revision": "5762f023a8a018959753",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/24.6f3341df.chunk.js"
+  },
+  {
+    "revision": "9a3e6fffff683c37b39a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/25.1fef081e.chunk.js"
+  },
+  {
+    "revision": "4c3167f89cf61107591d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/26.835d0f61.chunk.js"
+  },
+  {
+    "revision": "de3749962d481ca3b515",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/27.83ad2882.chunk.js"
+  },
+  {
+    "revision": "af5389b1f36b874f767f",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/28.5c2c02da.chunk.js"
+  },
+  {
+    "revision": "ed0f97eb244cb333638c",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/29.fe20313a.chunk.js"
+  },
+  {
+    "revision": "fa1780e60bb4a42c7cb1",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/3.22b6db31.chunk.js"
+  },
+  {
+    "revision": "73aa0a6b0a1cddbb1336",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/30.56ea64ac.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/30.56ea64ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09f8fe58f9c4d73578a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/31.86205c4a.chunk.js"
+  },
+  {
+    "revision": "b151254f8ecab77716a8",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/32.24d51d1e.chunk.js"
+  },
+  {
+    "revision": "a2ad9a86e2154f78a610",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/33.50e97442.chunk.js"
+  },
+  {
+    "revision": "0df3c977731a9f5329d9",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/34.1c9078cc.chunk.js"
+  },
+  {
+    "revision": "eb2b15f1e18d7734867d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/35.d2705282.chunk.js"
+  },
+  {
+    "revision": "ca293169f94cd059acd6",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/36.d6cab4fb.chunk.js"
+  },
+  {
+    "revision": "8acf53cd4379ab25d03a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/37.7a689267.chunk.js"
+  },
+  {
+    "revision": "51bbd52d8d071a4382b3",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/38.1127b1bc.chunk.js"
+  },
+  {
+    "revision": "47fba555d7c3e0002ff7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/39.8ebfadf8.chunk.js"
+  },
+  {
+    "revision": "35efe00223807a1258c0",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/4.89cc48a3.chunk.js"
+  },
+  {
+    "revision": "2c2d0f372b2a2ab4bc78",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/40.df958efd.chunk.js"
+  },
+  {
+    "revision": "c31e9c34db87dc0ffe50",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/41.504ed4e0.chunk.js"
+  },
+  {
+    "revision": "d87fa195eaeb97c9fa12",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/42.23a27830.chunk.js"
+  },
+  {
+    "revision": "b825cd0063aa6b0a14f1",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/43.7dd63590.chunk.js"
+  },
+  {
+    "revision": "a8d427083288199d5c77",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/44.07137416.chunk.js"
+  },
+  {
+    "revision": "905d43488c39b4fd0c8b",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/45.87498947.chunk.js"
+  },
+  {
+    "revision": "a21935b2a57cfc589e9a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/46.23b1bceb.chunk.js"
+  },
+  {
+    "revision": "9bda641089d6a3d89e58",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/47.434a72ad.chunk.js"
+  },
+  {
+    "revision": "5b0f734a425b7df4f550",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/48.d2741363.chunk.js"
+  },
+  {
+    "revision": "1da18862591d2b227082",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/49.b10d6104.chunk.js"
+  },
+  {
+    "revision": "c1501a81df91c5e6183b",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/5.6f2d59c2.chunk.js"
+  },
+  {
+    "revision": "441f1bcee1c965fccfbb",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/50.42c1fe34.chunk.js"
+  },
+  {
+    "revision": "64a376e8af0190f3fb64",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/51.5c0a370e.chunk.js"
+  },
+  {
+    "revision": "03edcca619bd9f3c3db1",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/52.d49e9615.chunk.js"
+  },
+  {
+    "revision": "764979a42091935fe201",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/53.2aa593f3.chunk.js"
+  },
+  {
+    "revision": "796020f5a8a7383ca96e",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/54.6f6e374f.chunk.js"
+  },
+  {
+    "revision": "c744b8b8a3ef802b1568",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/55.b6e5086d.chunk.js"
+  },
+  {
+    "revision": "1c67d2ec26038209f097",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/56.c2597017.chunk.js"
+  },
+  {
+    "revision": "d80c7015e6641f4b00b4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/57.c67db3a8.chunk.js"
+  },
+  {
+    "revision": "b781bb903260c1716827",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/58.6a6e8a42.chunk.js"
+  },
+  {
+    "revision": "7ad889fbecfc22776ef8",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/59.dd69733f.chunk.js"
+  },
+  {
+    "revision": "b6184ac25c056f7d3e51",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/60.21c9a7af.chunk.js"
+  },
+  {
+    "revision": "a9ac67faf10282d3cc7a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/61.f0e1b554.chunk.js"
+  },
+  {
+    "revision": "5db562644f2633ed9c10",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/62.be9a9523.chunk.js"
+  },
+  {
+    "revision": "5f6d1bc7fcec0fd7fff7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/63.32ece931.chunk.js"
+  },
+  {
+    "revision": "f6bd257a24b2d9c7dc97",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/64.a1c60c60.chunk.js"
+  },
+  {
+    "revision": "f640d96672f1d61cb24d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/65.31f151e6.chunk.js"
+  },
+  {
+    "revision": "5215591562d00fc093bc",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/66.0fb0b198.chunk.js"
+  },
+  {
+    "revision": "db275989a080710eb221",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/67.83152831.chunk.js"
+  },
+  {
+    "revision": "0bdb7dca3731778f6ba7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/68.aea5d3cd.chunk.js"
+  },
+  {
+    "revision": "901feaa2a4439cb4bde7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/69.bfad1ceb.chunk.js"
+  },
+  {
+    "revision": "4ed6bb1b9155e9db372a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/70.51b299a2.chunk.js"
+  },
+  {
+    "revision": "ff917f529e2c1a145b58",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/71.5560914a.chunk.js"
+  },
+  {
+    "revision": "de7d495196c98567e0ca",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/72.6777a7a1.chunk.js"
+  },
+  {
+    "revision": "81751e7d914d5b27024c",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/73.b05bd272.chunk.js"
+  },
+  {
+    "revision": "995771a479b2536fd53d",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/74.159577c4.chunk.js"
+  },
+  {
+    "revision": "63be5a9f8b6df9af55c5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/75.775d788d.chunk.js"
+  },
+  {
+    "revision": "a6c63ab3f6abbc4dca58",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/76.fe61b8ec.chunk.js"
+  },
+  {
+    "revision": "b8ec0875826eae25d354",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/77.1db9ce53.chunk.js"
+  },
+  {
+    "revision": "addc01392661a06310b6",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/78.5ad5452a.chunk.js"
+  },
+  {
+    "revision": "e807d292aff0abfc6b4b",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/79.4723abc6.chunk.js"
+  },
+  {
+    "revision": "51ff675cd39f86824a77",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/8.22569fd5.chunk.js"
+  },
+  {
+    "revision": "0229f2113b78559ca1daeb1dd663a5a5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/8.22569fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49eb6b034afe47bc093",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/80.b57791a7.chunk.js"
+  },
+  {
+    "revision": "6d3aab8673b39cbea1fc",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/81.1826a786.chunk.js"
+  },
+  {
+    "revision": "25e251d4d85488d883b2",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/82.679f02b3.chunk.js"
+  },
+  {
+    "revision": "1cfe1001ff98250b0716",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/83.75e66955.chunk.js"
+  },
+  {
+    "revision": "de0c2aecf8d7e4c5fe64",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/84.6afb1842.chunk.js"
+  },
+  {
+    "revision": "f55ca9d2d92f1dd0fb5a",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/85.dc7aad20.chunk.js"
+  },
+  {
+    "revision": "cbdb1a317f0870d0be05",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/86.4116e58e.chunk.js"
+  },
+  {
+    "revision": "8c0a5caaf57afff74e37",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/87.a2dd98fa.chunk.js"
+  },
+  {
+    "revision": "2f90788b85e59eda7163",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/88.f7cd4dbf.chunk.js"
+  },
+  {
+    "revision": "016ee3c08b435a3751eb",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/89.acd8fac0.chunk.js"
+  },
+  {
+    "revision": "6831e0684a9779a1b319",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/9.4301c7c0.chunk.js"
+  },
+  {
+    "revision": "ccadfa4452e28bd3c5a2",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/90.f33254ef.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/90.f33254ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155918918297cfb48390",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/91.ebc7134f.chunk.js"
+  },
+  {
+    "revision": "88b2f38d8d5968e97b87",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/92.d8420830.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/92.d8420830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325c1a8ea12af7514ba0",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/93.5561fef2.chunk.js"
+  },
+  {
+    "revision": "d43d8d86be2c51f609b4",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/94.ca1cfc0c.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/94.ca1cfc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b9de0aacf738ae3b4a7",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/main.c4b81b14.chunk.js"
+  },
+  {
+    "revision": "d1aa60ae34c6b1c86c57",
+    "url": "https://swipe-the-curve.github.io/swipe-the-curve/forecast/static/js/runtime-main.fa58959e.js"
+  }
+]);
