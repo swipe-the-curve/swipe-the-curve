7214b3ac9758e1559c568cca9e0bcd650f14679e
Lennart Hensler <lennarthensler@gmail.de>
2020-03-22 10:33:44 +0100

    Clean curve and adjust line colors

--- src/components/Curve.tsx
+++ src/components/Curve.tsx
@@ -2,55 +2,21 @@ import { } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
 import { ResponsiveContainer, AreaChart, Area, Tooltip, } from 'recharts';
 import { game } from '../core';
-import { EpidemicState } from '../core/epidemicmodel';
-
-const data = [
-  {
-    name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-  },
-];
-
-const mapStates = (epidemicStates: EpidemicState[]) => {
-  const mappedEpidemicState = epidemicStates.map(epidemicState => {
-    const entry = {
-      infected: epidemicState.infected,
-      recovered: epidemicState.recovered,
-      dead: epidemicState.dead,
-    };
-
-
-    return entry;
-  })
-  return mappedEpidemicState;
-}
 
 const Curve: React.FC = () => {
-  const mappedEpidemicState = mapStates(game.epidemicStates);
-  console.log(mappedEpidemicState);
-  const [epidemicState, setEpidemicState] = useState(mappedEpidemicState);
+  const [epidemicState, setEpidemicState] = useState();
 
   useEffect(() => {
     console.log("addEventlistener");
     game.addStateListener(() => {
-      const mappedEpidemicState = mapStates(game.epidemicStates);
+      const mappedEpidemicState = game.epidemicStates.map((epidemicState, index) => {
+        return {
+          name: `Tag ${index + 1}`,
+          infected: epidemicState.infected,
+          recovered: epidemicState.recovered,
+          dead: epidemicState.dead,
+        };
+      })
       setEpidemicState(mappedEpidemicState);
     });
   }, [])
@@ -67,9 +33,9 @@ const Curve: React.FC = () => {
             }}
           >
             <Tooltip />
-            <Area isAnimationActive={false} type="monotone" dataKey="infected" stackId="1" stroke="#8884d8" fill="#8884d8" />
-            <Area isAnimationActive={false} type="monotone" dataKey="recovered" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-            <Area isAnimationActive={false} type="monotone" dataKey="dead" stackId="1" stroke="#ffc658" fill="#ffc658" />
+            <Area isAnimationActive={false} type="monotone" dataKey="infected" stackId="1" stroke="#f53d3d" fill="#f53d3d" />
+            <Area isAnimationActive={false} type="monotone" dataKey="recovered" stackId="1" stroke="#4db374" fill="#4db374" />
+            <Area isAnimationActive={false} type="monotone" dataKey="dead" stackId="1" stroke="#3b1111" fill="#3b1111" />
           </AreaChart>
         </ResponsiveContainer>
     </div>
