85738b8bbf5c8e78d717c1283933077a97dcd55e
Dr. Corona <dr.corona@the-imperfection.de>
2020-03-22 10:28:18 +0000

    Deploying to gh-pages from  - 9cee2409a01e147c85f5fd047bde613b35c46757 🚀

--- master/service-worker.js
+++ master/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://swipe-the-curve.github.io/swipe-the-curve/master/precache-manifest.0389b9bc3f870d37943b51e6770788ea.js"
+  "https://swipe-the-curve.github.io/swipe-the-curve/master/precache-manifest.dc59fe9f590d9aa7f4238b980a658522.js"
 );
 
 self.addEventListener('message', (event) => {
