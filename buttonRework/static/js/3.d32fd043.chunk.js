e06252a5a46fcc439a362c0a430d41d2c0357be3
Dr. Corona <dr.corona@the-imperfection.de>
2020-03-22 10:12:43 +0000

    Deploying to gh-pages from  - a7559651ef71dd933ab3d18aa4e4b257613dff31 🚀

---
+++ buttonRework/static/js/3.d32fd043.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpswipe-the-curve"]=this["webpackJsonpswipe-the-curve"]||[]).push([[3],{490:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(58),o=(e(11),function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.g)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.l)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=3.d32fd043.chunk.js.map
