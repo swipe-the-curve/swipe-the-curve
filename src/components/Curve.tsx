a7bf2d8b22401d9b533e4fc7266f1cd645ba2384
Frederik Petersen <frederik@the-imperfection.de>
2020-03-22 16:20:08 +0100

    Use total impacted for condition whether to show capacity or not

--- src/components/Curve.tsx
+++ src/components/Curve.tsx
@@ -1,6 +1,6 @@
 import { } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
-import { ResponsiveContainer, Area, Tooltip, ComposedChart, ReferenceLine} from 'recharts';
+import { ResponsiveContainer, Area, Tooltip, ComposedChart, Line} from 'recharts';
 import { game } from '../core';
 
 const Curve: React.FC = () => {
@@ -31,7 +31,7 @@ const Curve: React.FC = () => {
   console.log("EpidState: ", epidemicState);
 
   const currentCapacity = epidemicState && epidemicState.length > 0 && epidemicState[epidemicState.length - 1].capacity || 0;
-  const currentInfected = epidemicState && epidemicState.length > 0 && epidemicState[epidemicState.length - 1].infected || 0;
+  const currentImpacted = epidemicState && epidemicState.length > 0 && epidemicState[epidemicState.length - 1].infected + epidemicState[epidemicState.length - 1].recovered + epidemicState[epidemicState.length - 1].dead || 0;
 
   return (
     <div className="full-width">
@@ -47,8 +47,8 @@ const Curve: React.FC = () => {
             <Area isAnimationActive={false} type="monotone" dataKey="infected" stackId="1" stroke="#f53d3d" fill="#f53d3d" />
             <Area isAnimationActive={false} type="monotone" dataKey="recovered" stackId="1" stroke="#4db374" fill="#4db374" />
             <Area isAnimationActive={false} type="monotone" dataKey="dead" stackId="1" stroke="#3b1111" fill="#3b1111" />
-            {currentCapacity && (currentInfected * 8 >= currentCapacity) && 
-              <ReferenceLine y={currentCapacity} label='Healthcare capacity' stroke="black" strokeDasharray="3 3" alwaysShow/>
+            {currentCapacity && (currentImpacted * 8 >= currentCapacity) && 
+              <Line isAnimationActive={false} dataKey="capacity" type="monotone" stroke="black" strokeDasharray="3 3" dot={false} />
             }
           </ComposedChart>
         </ResponsiveContainer>
